97a1ac1a7d3a789576cf866860716a3d398aa9db
Loisa Kitakaya <kitakayaloisa@gmail.com>
2025-06-28 11:45:37 +0300

    configured tenant authentication

--- tenants/urls.py
+++ tenants/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login", views.sign_in, name="login"),
+    path("logout", views.sign_out, name="logout"),
 ]
--- tenants/views.py
+++ tenants/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
     context = {}
 
     return render(request, "tenants/home.html", context)
+
+
+def sign_in(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            messages.success(request, "Successfully authenticated credentials")
+
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password")
+
+    context = {}
+
+    return render(request, "auth/login.html", context)
+
+
+def sign_out(request):
+    logout(request)
+
+    return redirect("login")
