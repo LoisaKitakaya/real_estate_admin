965c1d70c068d0e8c1d09b70859dcdce18f9465e
Loisa Kitakaya <kitakayaloisa@gmail.com>
2025-07-04 14:18:10 +0300

    minor updated

--- tenants/urls.py
+++ tenants/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("logout", views.sign_out, name="logout"),
     path("profile", views.profile, name="profile"),
     path("payments", views.payments, name="payments"),
+    path("payments/make_payment", views.make_payment, name="make_payment"),
+    path("payments/stripe_webhook", views.stripe_webhook, name="webhook"),
+    path("payments/success", views.success, name="success"),
     path("maintenance", views.maintenance, name="maintenance"),
 ]
--- tenants/views.py
+++ tenants/views.py
@@ -1,12 +1,25 @@
+import uuid
+import stripe
+import logging
 from lease.models import Lease
+from django.conf import settings
 from tenants.models import Tenant
 from finance.models import Payment
 from django.contrib import messages
 from property.models import Property
+from datetime import timedelta, date
 from django.shortcuts import render, redirect
 from maintenance.models import MaintenanceRequest
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.http import require_http_methods
+from utils.stripe import create_payment_link, retrieve_payment_intent
+
+
+logger = logging.getLogger(__name__)
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def home(request):
@@ -118,3 +131,77 @@ def maintenance(request):
     }
 
     return render(request, "tenants/maintenance.html", context)
+
+
+@login_required(login_url="/login")
+@require_http_methods(["POST"])
+def make_payment(request):
+    current_user = request.user
+
+    tenant = Tenant.objects.get(user=current_user)
+
+    lease = Lease.objects.get(tenant=tenant)
+
+    url = create_payment_link(lease)
+
+    return redirect(url)
+
+
+def success(request):
+    return render(request, "components/success.html")
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
+
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError:
+        return JsonResponse({"status": "invalid payload"}, status=400)
+    except stripe.error.SignatureVerificationError:
+        return JsonResponse({"status": "invalid signature"}, status=400)
+
+    if event["type"] == "checkout.session.completed":
+        checkout_session = event["data"]["object"]
+
+        print(f"Payment was successful! Checkout session object: {checkout_session}")
+
+        try:
+            assert checkout_session["payment_status"] == "paid"
+        except Exception as e:
+            print(f"Payment not completed: {str(e)}")
+
+            return JsonResponse({"status": "failed"}, status=400)
+
+        lease = Lease.objects.get(
+            id=uuid.UUID(checkout_session["metadata"]["lease_id"])
+        )
+
+        payment = Payment.objects.create(
+            lease=lease,
+            amount=(int(checkout_session["amount_total"]) / 100),
+            payment_date=date.today(),
+        )
+
+        payment.status = "paid"
+
+        payment.save()
+
+        previous_deadline = lease.end_date
+
+        next_deadline = previous_deadline + timedelta(days=30)
+
+        lease.start_date = previous_deadline
+
+        lease.end_date = next_deadline
+
+        lease.save()
+
+    return JsonResponse({"status": "success"}, status=200)
